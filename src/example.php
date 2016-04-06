61d437371995410c9740ca598a708bba542405ac
Зотов Максим < >
2016-04-06 23:58:47 +0300

    add example

--- src/example.php
+++ src/example.php
@@ -2,3 +2,15 @@
 
 require __DIR__.'/vendor/autoload.php';
 
+use zotov_mv\Logger\Logger;
+use zotov_mv\Logger\Handlers\NullHandler;
+use zotov_mv\Logger\Handlers\StreamHandler;
+use zotov_mv\Logger\Formatter\Line as LineFormatter;
+
+$streamHandler =  new StreamHandler('E:\project\check_list\my\logger\src\log.log');
+$streamHandler->setFormatter(new LineFormatter());
+
+
+$logger = new Logger($streamHandler);
+
+
