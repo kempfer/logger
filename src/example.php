36dec5ebe64f81fed83e7d1f618e186e6844c08a
Зотов Максим < >
2016-07-17 22:45:45 +0300

    change file example

--- src/example.php
+++ src/example.php
@@ -2,15 +2,21 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use zotov_mv\Logger\Logger;
-use zotov_mv\Logger\Handlers\NullHandler;
-use zotov_mv\Logger\Handlers\StreamHandler;
+use zotov_mv\Logger\LoggerManager;
+use zotov_mv\Logger\Loggers\Stream as StreamLogger;
 use zotov_mv\Logger\Formatter\Line as LineFormatter;
 
-$streamHandler =  new StreamHandler('E:\project\check_list\my\logger\src\log.log');
-$streamHandler->setFormatter(new LineFormatter());
+$stream = new StreamLogger('E:\project\check_list\my\logger\src\log.log');
+
+$loggerAware = new LoggerManager($stream, 'dev', new DateTimeZone('Europe/Zaporozhye'));
 
 
-$logger = new Logger($streamHandler);
+$stream->setDeferredRecord(true);
+$loggerAware->setLogger($stream);
+try {
+    throw new \Exception("Test");
+} catch(\Exception $e){
+    $loggerAware->getLogger()->alert('test message {name}', ['name' => 'maxim', 'except' => $e]);
+}
 
 
